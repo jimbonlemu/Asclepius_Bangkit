026fc5ff46763304aa52f0064fe3afec224e7d8f
perompak_kode <iqbaltech29@gmail.com>
2024-04-22 22:17:02 +0700

    Added splash screen with animations

--- app/src/main/java/com/dicoding/asclepius/view/EntryActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/EntryActivity.kt
@@ -1,14 +1,49 @@
 package com.dicoding.asclepius.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
+import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityEntryBinding
+import com.dicoding.asclepius.utils.ThemeSettingPreference
+import com.dicoding.asclepius.utils.datastore
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
 class EntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ActivityEntryBinding.inflate(layoutInflater).apply {
             setContentView(root)
+            initDarkMode()
+            ivSplash.startAnimation(
+                AnimationUtils.loadAnimation(
+                    this@EntryActivity, R.anim.anim_fade
+                )
+            )
+            lifecycleScope.launch {
+                delay(SPLASH_SCREEN_DURATION.seconds)
+                startActivity(Intent(this@EntryActivity, MainActivity::class.java))
+                finish()
+            }
         }
+    }
+
+    private fun initDarkMode() {
+        ThemeSettingPreference.getInstanceOfThemeSettingPreference(application.datastore).getSavedThemeSetting().asLiveData()
+            .observe(this) { darkModeActive ->
+                val setMode =
+                    if (darkModeActive) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+                AppCompatDelegate.setDefaultNightMode(setMode)
+            }
+    }
+
+    companion object {
+        const val SPLASH_SCREEN_DURATION = 3
     }
 }
