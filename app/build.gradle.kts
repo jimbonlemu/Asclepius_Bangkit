cd49cd3d707bea9774b50cd3f75f67cc87bdb27a
jimbonlemu <iqbaltech29@gmail.com>
2024-12-23 16:20:27 +0700

    Update dependencies in build.gradle.kts:
    - Updated Datastore library version to 1.1.0
    - Updated Activity KTX library version to 1.9.0
    - Moved Glide library under Image Library section

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,29 +60,25 @@ dependencies {
     implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
     implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 
-//  Image Cropper Library
+//  Image Library
     implementation("com.github.yalantis:ucrop:2.2.8")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
 //  Datastore Library
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.0")
 
 //  Room Library
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
 
 //  API Library
     implementation("com.squareup.retrofit2:retrofit:2.10.0")
     implementation("com.squareup.retrofit2:converter-gson:2.10.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
-//  Component Library
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-
 }
