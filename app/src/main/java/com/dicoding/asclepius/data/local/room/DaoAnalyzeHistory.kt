1780580aaaf71b91c417a21750eef6d2c7c0794b
jimbonlemu <iqbaltech29@gmail.com>
2024-12-23 16:24:44 +0700

    Format DaoAnalyzeHistory.kt:
    - Removed unnecessary indentation at the beginning of the file for improved code style and consistency.

--- app/src/main/java/com/dicoding/asclepius/data/local/room/DaoAnalyzeHistory.kt
+++ app/src/main/java/com/dicoding/asclepius/data/local/room/DaoAnalyzeHistory.kt
@@ -1,17 +1,17 @@
-    package com.dicoding.asclepius.data.local.room
+package com.dicoding.asclepius.data.local.room
 
-    import androidx.lifecycle.LiveData
-    import androidx.room.Dao
-    import androidx.room.Insert
-    import androidx.room.OnConflictStrategy
-    import androidx.room.Query
-    import com.dicoding.asclepius.data.local.entity.EntityAnalyzeHistory
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.dicoding.asclepius.data.local.entity.EntityAnalyzeHistory
 
-    @Dao
-    interface DaoAnalyzeHistory {
-        @Query("SELECT * FROM EntityAnalyzeHistory")
-        fun getAllAnalyzeHistory(): LiveData<List<EntityAnalyzeHistory>>
+@Dao
+interface DaoAnalyzeHistory {
+    @Query("SELECT * FROM EntityAnalyzeHistory")
+    fun getAllAnalyzeHistory(): LiveData<List<EntityAnalyzeHistory>>
 
-        @Insert(onConflict = OnConflictStrategy.IGNORE)
-        suspend fun insertAnalyzeHistory(entityAnalyzeHistory: EntityAnalyzeHistory)
-    }
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertAnalyzeHistory(entityAnalyzeHistory: EntityAnalyzeHistory)
+}
